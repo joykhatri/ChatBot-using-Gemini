571fb9b615bda5bbe6e8b3d1757232ef701dd5e8
joykhatri <joykhatri5@gmail.com>
2026-03-25 19:16:17 +0530

    Update urls.py

--- gemini_chatbot_project/urls.py
+++ gemini_chatbot_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from gemini_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', chat_viewset)
+    path('chat/', chat_viewset),
+    path('api/', include('gemini_app.urls'))
 ]
