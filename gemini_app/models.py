8d0bf7fc0ab847597c348557c598fc81c9c9a345
joykhatri <joykhatri5@gmail.com>
2026-03-25 19:13:05 +0530

    Add files via upload

--- gemini_app/models.py
+++ gemini_app/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = None
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True, default='example@gmail.com')
+    password = models.CharField(max_length=150)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.name
 
 class Chat(models.Model):
     user = models.TextField()
@@ -7,3 +20,4 @@ class Chat(models.Model):
 
     def __str__(self):
         return self.user
+    
--- gemini_app/serializers.py
+++ gemini_app/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
 from gemini_app.models import *
+from django.contrib.auth.hashers import make_password
 
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {'password' : {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            instance.password = make_password(validated_data['password'])
+            validated_data.pop('password')
+        return super().update(instance, validated_data)
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
